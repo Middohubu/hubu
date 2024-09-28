a8ad5a0ee449e2d11b4db1b1b53ffd67c24fcb88
Middohubu <wowmaid@gmail.com>
2024-09-29 00:15:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
@@ -5,8 +6,8 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Lets move past this can't you see im trying!";
-  gif.src = "https://media.giphy.com/media/3o7TKq0oNLk8ljH7vG/giphy.gif?cid=ecf05e47ichl002v8lacteunpzt0qpip77rt5vej6lmwiads&ep=v1_gifs_search&rid=giphy.gif&ct=g";
+  question.innerHTML = "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
 });
 
 // Make the No button move randomly on hover
@@ -25,12 +26,3 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
-
-
-
-
-
-
-
-
-
